03938d009ebcc01cb5fc8511f5a650e23bd4738a
zhouping <zhouping@lkhealth.cn>
2017-06-19 18:11:47 +0800

    add middleman deploy

--- config.rb
+++ config.rb
@@ -44,6 +44,19 @@ configure :build do
   # activate :gzip
 end
 
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'gh-pages' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 # Deploy Configuration
 # If you want Middleman to listen on a different port, you can set that below
 set :port, 4567
+
+
+
+
